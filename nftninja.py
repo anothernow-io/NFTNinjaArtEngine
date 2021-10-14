f62110d4aff51e94b5045ec6f16c58eb28234d4e
Pető Zoltán <bajgli@gmail.com>
2021-10-14 17:25:41 +0200

    added command line arguments

--- nftninja.py
+++ nftninja.py
@@ -1,94 +1,33 @@
-from PIL import Image
-import os, json
-import random
-import hashlib
-import shutil
-from time import strftime, gmtime
-from config import nucleotides, colors, AVAILABLE_COLORS, ITEM_NAME, IMAGE_SIZE
-from config import MAX_ITEMS_TO_GENERATE, GENERATE_METADATA, GENERATE_IMAGES
-from config import CLEAN_UP_BEFORE_GENERATE, EXTERNAL_URL, EDITION
-
-
-nucleotide_options = {nucl: [os.path.splitext(x)[0] for x in os.listdir('./layers/single/' + nucl)] for nucl in nucleotides if nucl != 'color'}
-nucleotide_options['color'] = AVAILABLE_COLORS
-
-
-def generate_sprite_config(dna):
-    ret = []
-    for ncl_name, ncl_value in dna.items():
-        if ncl_name == 'color':
-            for nucl in colors[ncl_value]:
-                img_name = dna[nucl]
-                ret.append([ncl_name, f'./layers/colors/{ncl_value}/{nucl}/{img_name}.png'])
-        else:
-            ret.append([ncl_name, f'./layers/single/{ncl_name}/{ncl_value}.png'])
-    return ret
-
-
-def build_image(id, sprite_config):
-    base_image = Image.open('./layers/single/background/Pink.png')
-    for i, sprite in enumerate(sprite_config):
-        next_image = Image.open(sprite[1])
-        base_image.paste(next_image, (0, 0), next_image)
-    result = base_image.resize(IMAGE_SIZE)
-    print(f"./build/images/{id}.png")
-    result.save(f"./build/images/{id}.png")
-
-
-def hash_dna(dna):
-    return hashlib.sha1(json.dumps(dna, sort_keys=True).encode('UTF-8')).hexdigest()
-
-def generate_metadata(id, dna):
-    attributes = []
-    for d in dna:
-        attributes.append({'trait_type': d, 'value': dna[d]})
-
-    date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
-    metadata = {
-        'dna': hash_dna(dna),
-        'name': f'{ITEM_NAME} #{id}',
-        'description': "Schrödinger's cat who wants to live",
-        'image': f'{EXTERNAL_URL}/images/{id}.png',
-        'edition': f'{EDITION}',
-        'date': f'{date}',
-        'attributes': attributes,
-        'engine': 'NFTNinja v0.1'
-    }
-    return metadata
-
-def init_before_generate():
-    if not CLEAN_UP_BEFORE_GENERATE:
-        return
-    if os.path.exists('./build'):
-        print("Removing bulid folder ...")
-        shutil.rmtree('./build')
-    print("Creating build folder ...")
-    os.mkdir('./build')
-    os.mkdir('./build/images')
-    os.mkdir('./build/metadata')
-
-dnas = []
-existing_dna_hashes = []
+import argparse
+from nftninja.manager import Manager
+import config
 
 
 if __name__ == "__main__":
-    init_before_generate()
-    for d in range(MAX_ITEMS_TO_GENERATE):
-        dnax = {}
-        for n in nucleotides:
-            dnax[n] = nucleotide_options[n][random.randint(0, len(nucleotide_options[n]) - 1)]
-        h = hash_dna(dnax)
-        if h in existing_dna_hashes:
-            print("DNA exists, skipping...")
-        else:
-            existing_dna_hashes.append(h)
-            dnas.append(dnax)
-    for idx, dd in enumerate(dnas, start=1):
-        if GENERATE_METADATA:
-            metadata = generate_metadata(idx, dd)
-            with open(f'build/metadata/{idx}.json', 'w', encoding='utf-8') as f:
-                json.dump(metadata, f, ensure_ascii=False, indent=4)
-        if GENERATE_IMAGES:
-            scf = generate_sprite_config(dd)
-            build_image(idx, scf)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--edition", dest="edition", type=int, help="Edition number", default=1)
+    parser.add_argument("-in", "--item-name", dest="item_name", help="Name of the items in metadata JSON", default="NFTNinja")
+    parser.add_argument("-imax", "--items-max-number", dest="imax", type=int, help="Maximum number of items to generate", default=5)
+    parser.add_argument("-rg", "--regenerate", dest="regenerate", action='store_true', help="Regegerate build folder")
+    parser.add_argument("-nbi", "--no-build-images", dest="build_images", action='store_false', help="Build images")
+    parser.add_argument("-nbm", "--no-build-metadata", dest="build_metadata", action='store_false', help="Build JSON metadata")
+    parser.add_argument("-u", "--external-url", dest="external_url", help="External URL where images and metadata will be stored")
+    parser.add_argument("-r", "--run", dest="run", action='store_true', help="Determines if NFTNinja can run or not. If it is not supplied, program will not generate any output")
+    args = parser.parse_args()
+    
+    config.EDITION = args.edition
+    config.ITEM_NAME = args.item_name
+    config.MAX_ITEMS_TO_GENERATE = args.imax
+    config.CLEAN_UP_BEFORE_GENERATE = args.regenerate
+    config.GENERATE_IMAGES = args.build_images
+    config.GENERATE_METADATA = args.build_metadata
+    config.EXTERNAL_URL = args.external_url
+    
+    print(args)
+    if args.run:
+        m = Manager(config)
+
+        if args.regenerate:
+            m.init_before_generate()
+        m.generate_dnas()
+        m.run()
---
+++ nftninja/manager.py
@@ -0,0 +1,100 @@
+import shutil, os, hashlib, json
+from PIL import Image
+from typing import List
+from time import strftime, gmtime
+import random
+
+class Manager:
+    def __init__(self, config) -> None:
+        self.config = config
+        self.dnas = []
+        self.existing_dna_hashes = []
+        self.nucleotide_options = {nucl: [os.path.splitext(x)[0] for x in os.listdir('./layers/single/' + nucl)] for nucl in self.config.nucleotides if nucl != 'color'}
+        self.nucleotide_options['color'] = self.config.AVAILABLE_COLORS
+
+    def init_before_generate(self):
+        if not self.config.CLEAN_UP_BEFORE_GENERATE:
+            return
+        if os.path.exists('./build'):
+            print("Removing bulid folder ...")
+            shutil.rmtree('./build')
+        print("Creating build folder ...")
+        os.mkdir('./build')
+        os.mkdir('./build/images')
+        os.mkdir('./build/metadata')
+
+    def hash_dna(self, dna):
+        """
+        Hash of a DNA
+        """
+        return hashlib.sha1(json.dumps(dna, sort_keys=True).encode('UTF-8')).hexdigest()
+
+    def generate_sprite_config(self, dna):
+        """
+        Generates sprite config from a DNA
+        """
+        ret = []
+        for ncl_name, ncl_value in dna.items():
+            if ncl_name == 'color':
+                for nucl in self.config.colors[ncl_value]:
+                    img_name = dna[nucl]
+                    ret.append([ncl_name, f'./layers/colors/{ncl_value}/{nucl}/{img_name}.png'])
+            else:
+                ret.append([ncl_name, f'./layers/single/{ncl_name}/{ncl_value}.png'])
+        return ret
+
+    def build_image(self, id: int, sprite_config: List):
+        """
+        Generating image and saving it to a target directory
+        """
+        base_image = Image.open('./layers/single/background/Pink.png')
+        for i, sprite in enumerate(sprite_config):
+            next_image = Image.open(sprite[1])
+            base_image.paste(next_image, (0, 0), next_image)
+        result = base_image.resize(self.config.IMAGE_SIZE)
+        print(f"./build/images/{id}.png")
+        result.save(f"./build/images/{id}.png")
+
+    def build_metadata(self, id: int, dna: List):
+        """
+        Generating metadata from DNA
+        """
+        attributes = []
+        for d in dna:
+            attributes.append({'trait_type': d, 'value': dna[d]})
+
+        date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+        metadata = {
+            'dna': self.hash_dna(dna),
+            'name': f'{self.config.ITEM_NAME} #{id}',
+            'description': "Schrödinger's cat who wants to live",
+            'image': f'{self.config.EXTERNAL_URL}/images/{id}.png',
+            'edition': f'{self.config.EDITION}',
+            'date': f'{date}',
+            'attributes': attributes,
+            'engine': 'NFTNinja v0.1'
+        }
+        return metadata
+
+    def generate_dnas(self):
+        for d in range(self.config.MAX_ITEMS_TO_GENERATE):
+            dnax = {}
+            for n in self.config.nucleotides:
+                dnax[n] = self.nucleotide_options[n][random.randint(0, len(self.nucleotide_options[n]) - 1)]
+            h = self.hash_dna(dnax)
+            if h in self.existing_dna_hashes:
+                print("DNA exists, skipping...")
+            else:
+                self.existing_dna_hashes.append(h)
+                self.dnas.append(dnax)
+    
+    def run(self):
+        for idx, dd in enumerate(self.dnas, start=1):
+            if self.config.GENERATE_METADATA:
+                metadata = self.build_metadata(idx, dd)
+                with open(f'build/metadata/{idx}.json', 'w', encoding='utf-8') as f:
+                    json.dump(metadata, f, ensure_ascii=False, indent=4)
+            if self.config.GENERATE_IMAGES:
+                scf = self.generate_sprite_config(dd)
+                self.build_image(idx, scf)
