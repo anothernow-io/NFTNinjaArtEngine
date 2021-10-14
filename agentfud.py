85430404f9cca1b0f9892d039fb371d3b0db5022
Pető Zoltán <bajgli@gmail.com>
2021-10-14 23:10:51 +0200

    rarity config generation and image generation with rarity config json

--- agentfud.py
+++ agentfud.py
@@ -6,7 +6,7 @@ import config
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--edition", dest="edition", type=int, help="Edition number", default=1)
-    parser.add_argument("-in", "--item-name", dest="item_name", help="Name of the items in metadata JSON", default="NFTNinja")
+    parser.add_argument("-in", "--item-name", dest="item_name", help="Name of the items in metadata JSON", default="AgentFUD")
     parser.add_argument("-imax", "--items-max-number", dest="imax", type=int, help="Maximum number of items to generate", default=5)
     parser.add_argument("-rg", "--regenerate", dest="regenerate", action='store_true', help="Regegerate build folder")
     parser.add_argument("-nbi", "--no-build-images", dest="build_images", action='store_false', help="Build images")
@@ -15,7 +15,9 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--run", dest="run", action='store_true', help="Determines if NFTNinja can run or not. If it is not supplied, program will not generate any output")
     parser.add_argument("-g", "--generate-rarity-config", dest="generate_rarity_config", action='store_true', help="Generates rarity configuration template based on your configuration")
     parser.add_argument("-c", "--count-all-possibilities", dest="count_all_possibilities", action='store_true', help="Calculates all the possible combinations of your sprites based on your configuration")
+    parser.add_argument("-cr", "--check-rarities", dest="check_rarities", action='store_true', help="Checks difference between defined rarity configurations and real rarities")
     
+
     args = parser.parse_args()
     
     config.EDITION = args.edition
@@ -24,15 +26,17 @@ if __name__ == "__main__":
     config.CLEAN_UP_BEFORE_GENERATE = args.regenerate
     config.GENERATE_IMAGES = args.build_images
     config.GENERATE_METADATA = args.build_metadata
-    config.EXTERNAL_URL = args.external_url
+    config.EXTERNAL_URL = args.external_url if args.external_url is not None else config.EXTERNAL_URL
     
     print(args)
     m = Manager(config)
     
     if args.generate_rarity_config:
         m.generate_rarity_config()
-    if args.count_all_possibilities:
+    elif args.count_all_possibilities:
         m.count_all_possibilities()
+    elif args.check_rarities:
+        m.check_rarities()
     elif args.run:
         if args.regenerate:
             m.init_before_generate()
--- nftninja/manager.py
+++ nftninja/manager.py
@@ -73,31 +73,31 @@ class Manager:
             'edition': f'{self.config.EDITION}',
             'date': f'{date}',
             'attributes': attributes,
-            'engine': 'NFTNinja v0.1'
+            'engine': 'Agent FUD Art Engine v0.1'
         }
         return metadata
 
     def generate_dnas(self):
+        use_rarity_config = True
+        try:
+            with open(self.config.RARITY_JSON_NAME, "r") as file:
+                rarities = json.load(file)
+        except FileNotFoundError:
+            use_rarity_config = False
+
         for d in range(self.config.MAX_ITEMS_TO_GENERATE):
             dnax = {}
             for n in self.config.nucleotides:
-                dnax[n] = self.nucleotide_options[n][random.randint(0, len(self.nucleotide_options[n]) - 1)]
+                if use_rarity_config:
+                    dnax[n] = random.choices(self.nucleotide_options[n], weights=rarities[n].values(), k=1)[0]
+                else:
+                    dnax[n] = self.nucleotide_options[n][random.randint(0, len(self.nucleotide_options[n]) - 1)]
             h = self.hash_dna(dnax)
             if h in self.existing_dna_hashes:
                 print("DNA exists, skipping...")
             else:
                 self.existing_dna_hashes.append(h)
                 self.dnas.append(dnax)
-    
-    def run(self):
-        for idx, dd in enumerate(self.dnas, start=1):
-            if self.config.GENERATE_METADATA:
-                metadata = self.build_metadata(idx, dd)
-                with open(f'build/metadata/{idx}.json', 'w', encoding='utf-8') as f:
-                    json.dump(metadata, f, ensure_ascii=False, indent=4)
-            if self.config.GENERATE_IMAGES:
-                scf = self.generate_sprite_config(dd)
-                self.build_image(idx, scf)
 
     def generate_rarity_config(self):
         print("Generating rarity configuration")
@@ -107,13 +107,22 @@ class Manager:
             for gene in self.nucleotide_options[nucleotide]:
                 rarity_config[nucleotide][gene] = 1
 
-        with open(f'rarity-configuration.json', 'w', encoding='utf-8') as f:
+        with open(self.config.RARITY_JSON_NAME, 'w', encoding='utf-8') as f:
             json.dump(rarity_config, f, ensure_ascii=False, indent=4)
         print("Done...")
 
     def count_all_possibilities(self):
-        """
-        self.rarity_sprite_counts = [len(os.listdir(self.config.RARITY_LAYERS_FOLDER + '/' + i)) for i in self.config.LAYER_CONFIGURATIONS["rarity_layers"]]
-        self.max_possible_combinations = math.prod(self.rarity_sprite_counts)
-        """
-        print(10)
+        pass
+
+    def check_rarities(self):
+        pass
+
+    def run(self):
+        for idx, dd in enumerate(self.dnas, start=1):
+            if self.config.GENERATE_METADATA:
+                metadata = self.build_metadata(idx, dd)
+                with open(f'build/metadata/{idx}.json', 'w', encoding='utf-8') as f:
+                    json.dump(metadata, f, ensure_ascii=False, indent=4)
+            if self.config.GENERATE_IMAGES:
+                scf = self.generate_sprite_config(dd)
+                self.build_image(idx, scf)
